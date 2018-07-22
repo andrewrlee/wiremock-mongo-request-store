a87d8221d0e101901999b43f5c41c304a864d03f
Andy L <plasma147@gmail.com>
2018-07-22 18:41:30 +0100

    Adding a build script for the frontend

--- build-frontend.sh
+++ build-frontend.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e
+
 cd "$(dirname "$0")/frontend";
 
-ASSETS_DIR=src/main/resources/public/
+ASSETS_DIR=../src/main/resources/public/
 
 rm -Rf "$ASSETS_DIR"
 mkdir -p "$ASSETS_DIR"
 
 npm run build
 
-mv ./frontend/build/* "$ASSETS_DIR"
+mv ./build/* "$ASSETS_DIR"
