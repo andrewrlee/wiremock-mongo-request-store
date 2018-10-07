3787f53171928d8cca06c8a0983f9c802fff7919
Andy L <plasma147@gmail.com>
2018-10-07 16:15:38 +0100

    Making a multimodule gradle project

--- make-and-run.sh
+++ make-and-run.sh
@@ -7,7 +7,7 @@ echo 'Building frontend'
 ./frontend/build-frontend.sh 
 
 echo 'Building app';
-./gradlew --quiet build shadowjar
+./gradlew --quiet clean core:build core:shadowjar
 
 echo 'Running app';
 java -jar build/libs/wiremock-mongo-request-store-1.0-SNAPSHOT-all.jar src/main/resources/requests/
